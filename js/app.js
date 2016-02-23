606d1ffab70bae3d323e5d1be8b199399e27ed01
Alex Larson <alarson85@gmail.com>
2016-02-23 10:04:39 -0600

    finished in class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('friendsList', []);
+angular.module('friendsList', []);
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,6 @@
-var app = angular.module('friendsList');
-app.controller('mainCtrl', function($scope){
-	$scope.friends = ['Kevin', 'Maureen', 'Drew', 'Cesar'];
+angular.module('friendsList').controller('mainCtrl', function( $scope ){
+	$scope.name = 'Alex';
+	$scope.friends = ['Kevin', 'Maureen', 'Drew', 'Cesar', 'Jacob', 'Marcos', 'Brady', 'Rudey'];
 
 	$scope.addFriend = function(){
 		$scope.friends.push($scope.addName);
